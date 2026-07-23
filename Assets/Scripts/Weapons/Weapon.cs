db30f263967d1b8f46c665574b4cfb6d1a98ed47
SrslyKail <90731959+SrslyKail@users.noreply.github.com>
2026-07-22 19:30:28 -0700

    Add abstract Weapon class

---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,77 @@
+
+using System.Collections;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private InputReader inputReader;
+    [SerializeField] private Transform firePoint;
+    [Header("Settings")]
+    [SerializeField][Min(0f)] private float primaryCooldownSeconds;
+    [SerializeField][Min(0f)] private float secondaryCooldownSeconds;
+    [SerializeField] private float damage;
+    [SerializeField] private float fireCost;
+    
+    private bool _canFire;
+    
+    private Coroutine _fireCoroutine;
+    
+    private void Awake()
+    {
+        inputReader.PrimaryFireEvent += HandlePrimaryFire;
+        inputReader.SecondaryFireEvent += HandleSecondaryFire;
+    }
+
+    private void OnDestroy()
+    {
+        inputReader.PrimaryFireEvent -= HandlePrimaryFire;
+        inputReader.SecondaryFireEvent -= HandleSecondaryFire;
+        if (_fireCoroutine != null)
+        {
+            StopCoroutine(_fireCoroutine);
+        }
+    }
+
+    private void HandlePrimaryFire(bool shouldFire)
+    {
+        if (!shouldFire || !CanFireWeapon())
+        {
+            return;
+        }
+
+        PrimaryFireLogic();
+        _fireCoroutine = StartCoroutine(
+            StartFireTimer(primaryCooldownSeconds));
+    }
+
+    private void HandleSecondaryFire(bool shouldFire)
+    {
+        if (!shouldFire || !CanFireWeapon())
+        {
+            return;
+        }
+
+        SecondaryFireLogic();
+        _fireCoroutine = StartCoroutine(
+            StartFireTimer(secondaryCooldownSeconds));
+    }
+
+    private bool CanFireWeapon()
+    {
+        return (_fireCoroutine != null) && _canFire;
+    }
+
+    private IEnumerator StartFireTimer(float timerLength)
+    {
+        _canFire = false;
+        yield return new WaitForSeconds(timerLength);
+        _canFire = true;
+        _fireCoroutine = null;
+        
+    }
+
+    protected abstract void PrimaryFireLogic();
+
+    protected abstract void SecondaryFireLogic();
+}
